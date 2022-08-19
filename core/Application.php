02a81189ba3fd985f9fb0a8e0d40b3ce7d0d249a
nurkaydarov <nurkaydarov@yandex.kz>
2022-08-19 20:15:40 +0600

    Implements View and Layouts

--- core/Application.php
+++ core/Application.php
@@ -2,18 +2,25 @@
 namespace app\core;
 class Application
 {
+    //public static Application $app; // App instance for response
+    public static string $ROOT_PATH;
     public Router $router;
     public Request $request;
-    public function __construct()
+    public Response $response;
+    public function __construct($rootPath)
     {
+
+        self::$ROOT_PATH = $rootPath;
+        //self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
 
     }
 
     public function run()
     {
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 
 }
--- public/index.php
+++ public/index.php
@@ -1,15 +1,16 @@
 <?php
 require_once __DIR__ . "/../vendor/autoload.php";
 use app\core\Application;
-$app = new  Application();
+$app = new  Application(dirname(__DIR__));
 
-$app->router->get('/', function(){
+
+/*$app->router->get('/', function(){
     return 'Hello World';
-});
+});*/
 
-$app->router->get('/contact', function (){
-    return 'Contact';
-});
+$app->router->get('/', 'home');
+
+$app->router->get('/contact', 'contact');
 
 
 $app->run();
