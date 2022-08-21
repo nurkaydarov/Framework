823965e6dbbd8f16fde0f14a7bf2b93c391a26df
nurkaydarov <nurkaydarov@yandex.kz>
2022-08-21 13:30:30 +0600

    Sanitize Input Data

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,5 +1,6 @@
 <?php
 namespace app\controllers;
+use app\core\Application;
 use app\core\Controller;
 
 class SiteController extends Controller
@@ -10,9 +11,21 @@ class SiteController extends Controller
         $params = ['name' => 'Aktan Nurkaydarov'];
         return $this->render('home', $params);
     }
-
     public function contact()
     {
+        return $this->render('contact');
+    }
+
+    public function handleContact()
+    {
+       $body = Application::$app->request->getBody();
+        echo '<pre>';
+        var_dump($_POST);
+        echo '</pre>';
+       echo '<pre>';
+        var_dump($body);
+       echo '</pre>';
+
         return $this->render('contact');
     }
 }
--- public/index.php
+++ public/index.php
@@ -13,6 +13,6 @@ $app = new  Application(dirname(__DIR__));
 $app->router->get('/', [SiteController::class, 'home']);
 
 $app->router->get('/contact', [SiteController::class, 'contact']);
-$app->router->post('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
 
 $app->run();
